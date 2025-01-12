7976b0eb1b87d51242fdbbcd76cabe8097ee7ee8
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2025-01-12 13:39:24 +0600

    Add

--- FrontEnd/database/DataShirt.js
+++ FrontEnd/database/DataShirt.js
@@ -1,69 +1,69 @@
 const Items = [
     {
         id: '001',
-        item_img : `Images/Hoodies/H-1.jpg`,
+        item_img : `Images/Shirt/s-1.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
       {
         id: '002',
-        item_img : `Images/Hoodies/H-2.jpg`,
+        item_img : `Images/Shirt/s-2.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
         id: '003',
-        item_img : `Images/Hoodies/H-3.jpg`,
+        item_img : `Images/Shirt/s-3.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
         id: '004',
-        item_img : `Images/Hoodies/H-4.jpg`,
+        item_img : `Images/Shirt/s-4.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
         id: '005',
-        item_img : `Images/Hoodies/H-5.jpg`,
+        item_img : `Images/Shirt/s-5.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
         id: '006',
-        item_img : `Images/Hoodies/H-6.jpg`,
+        item_img : `Images/Shirt/s-6.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
         id: '007',
-        item_img : `Images/Hoodies/H-7.jpeg`,
+        item_img : `Images/Shirt/s-7.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
-        id: '008',
-        item_img : `Images/Hoodies/H-8.jpg`,
+        id: '007',
+        item_img : `Images/Shirt/s-8.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
         id: '009',
-        item_img : `Images/Hoodies/H-9.jpg`,
+        item_img : `Images/Shirt/s-9.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
 
       {
         id: '010',
-        item_img : `Images/Hoodies/H-10.jpg`,
+        item_img : `Images/Shirt/s-10.jpg`,
         item_Name : `Women's Casual Top`,
         item_Price : `Tk : 300`
       },
