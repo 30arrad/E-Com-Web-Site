e72234c4a5e460740411514691361deeecb3abf9
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2025-01-13 18:58:41 +0600

    Add

---
+++ Other File/ModelTast/DataT_Shirts.js
@@ -0,0 +1,12 @@
+const Items = [
+    { id: '001', item_img: 'Images/Hoodies/H-1.jpg', item_Name: 'Mens Premium T-Shirt- Jamming 1', item_Price: '৳ 599' },
+    { id: '002', item_img: 'Images/T-Shirts/t-2.jpg', item_Name: 'Mens Premium T-Shirt - Nothing Less 2', item_Price: '৳ 599' },
+    { id: '003', item_img: 'Images/T-Shirts/t-3.jpg', item_Name: 'Fabrilife Mens Premium T-Shirt - Nasa 3', item_Price: '৳ 599' },
+    { id: '004', item_img: 'Images/T-Shirts/t-4.jpg', item_Name: 'Fabrilife Mens Premium T-shirt - Maze 4', item_Price: '৳ 599' },
+    { id: '005', item_img: 'Images/T-Shirts/t-5.jpg', item_Name: 'Mens Premium Classic T-Shirt - White Liner 5', item_Price: '৳ 599' },
+    { id: '006', item_img: 'Images/T-Shirts/t-6.jpg', item_Name: 'Mens Premium T-Shirt - Blackrock 6', item_Price: '৳ 599' },
+    { id: '007', item_img: 'Images/T-Shirts/t-7.jpg', item_Name: 'Mens Premium T-Shirt- Shine 7', item_Price: '৳ 599' },
+    { id: '008', item_img: 'Images/T-Shirts/t-8.jpg', item_Name: 'Mens Premium T-Shirt- Threshold 8', item_Price: '৳ 599' },
+    { id: '009', item_img: 'Images/T-Shirts/t-9.jpg', item_Name: 'Mens Premium T-Shirt - Euphrates 9', item_Price: '৳ 599' },
+    { id: '010', item_img: 'Images/T-Shirts/t-10.jpg', item_Name: 'Fabrilife Mens Premium T-shirt - Destruction 10', item_Price: '৳ 599' },
+];
