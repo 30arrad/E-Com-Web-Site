1f36ef54b13ed5021773dc301df6988f3aca6af3
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2025-01-13 18:15:30 +0600

    Add

--- FrontEnd/Index.js
+++ FrontEnd/Index.js
@@ -1,16 +1,12 @@
 // console.log("Arrad ");
+AddAllItems();
+AllItemsModals()
+
+
+
+function AddAllItems()  {
 
 let Hoodies = document.getElementById('items-Hoodies');
-
-// console.log(Hoodies);
-
-// const Item = {
-//   id: '001',
-//   item_img : `Images/Hoodies/H-1.jpg`,
-//   item_Name : `Women's Casual Top`,
-//   item_Price : `Tk : 300`
-// }
-
 let innerHTML = ``;
 Items.forEach( Items => {
   innerHTML += `
@@ -20,11 +16,51 @@ Items.forEach( Items => {
       </a>
       <h3 class="font-bold mt-4">${Items.item_Name}</h3>
       <p class="text-gray-500 mt-1">${Items.item_Price}</p>
-      <button class="mt-4 w-full bg-gray-800 text-white py-2 rounded hover:bg-gray-700">Wishlist</button>
-      <button class="mt-4 w-full bg-gray-800 text-white py-2 rounded hover:bg-gray-700">Add to cart</button>
+      <button id="Wishlist" class="mt-4 w-full bg-gray-800 text-white py-2 rounded hover:bg-gray-700">Wishlist</button>
+      <button id="AddToCart" class="mt-4 w-full bg-gray-800 text-white py-2 rounded hover:bg-gray-700">Add to cart</button>
     </div>
   `
 });
 
 Hoodies.innerHTML = innerHTML;
+
+};
+
+
+function AllItemsModals() {
+  let Hoodies = document.getElementById('AddToCart');
+let modal = document.getElementById('modal');
+let closeModalBtn = document.getElementById('closeModalBtn');
+let cancelBtn = document.getElementById('cancelBtn');
+
+
+// Open modal function
+function openModal() {
+  modal.classList.remove('hidden');
+}
+
+// Close modal function
+function closeModal() {
+  modal.classList.add('hidden');
+}
+
+// Event listeners
+Hoodies.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
+cancelBtn.addEventListener('click', closeModal);
+
+
+    // Close modal when clicking outside modal content
+    modal.addEventListener('click', (event) => {
+      if (event.target === modal) closeModal();
+    });
+}
+
+
+
+
+
+
+
+
 
--- FrontEnd/database/DataT_Shirts.js
+++ FrontEnd/database/DataT_Shirts.js
@@ -2,70 +2,79 @@ const Items = [
     {
         id: '001',
         item_img : `Images/T-Shirts/t-1.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Mens Premium T-Shirt- Jamming`,
+        item_Price : `৳ 599`,
+
       },
       {
         id: '002',
         item_img : `Images/T-Shirts/t-2.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Mens Premium T-Shirt - Nothing Less`,
+        item_Price : `৳ 599`,
+
       },
 
       {
         id: '003',
         item_img : `Images/T-Shirts/t-3.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Fabrilife Mens Premium T-Shirt - Nasa`,
+        item_Price : `৳ 599`,
+
       },
 
       {
         id: '004',
         item_img : `Images/T-Shirts/t-4.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Fabrilife Mens Premium T-shirt - Maze`,
+        item_Price : `৳ 599`,
+
       },
 
       {
         id: '005',
         item_img : `Images/T-Shirts/t-5.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Mens Premium Classic T-Shirt - White Liner`,
+        item_Price : `৳ 599`,
+
       },
 
       {
         id: '006',
         item_img : `Images/T-Shirts/t-6.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Mens Premium T-Shirt - Blackrock`,
+        item_Price : `৳ 599`,
       },
 
       {
         id: '007',
         item_img : `Images/T-Shirts/t-7.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Mens Premium T-Shirt- Shine`,
+        item_Price : `৳ 599`,
+
       },
 
       {
-        id: '007',
+        id: '008',
         item_img : `Images/T-Shirts/t-8.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Mens Premium T-Shirt- Threshold`,
+        item_Price : `৳ 599`,
+
       },
 
       {
         id: '009',
         item_img : `Images/T-Shirts/t-9.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Mens Premium T-Shirt - Euphrates`,
+        item_Price : `৳ 599`,
+  
       },
 
       {
         id: '010',
         item_img : `Images/T-Shirts/t-10.jpg`,
-        item_Name : `Women's Casual Top`,
-        item_Price : `Tk : 300`
+        item_Name : `Fabrilife Mens Premium T-shirt - Destruction`,
+        item_Price : `৳ 599`,
+
       },
 
 ];
